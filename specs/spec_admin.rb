b2adbf42573fa28f03c0f54cf85c734b94b303d4
Abiaina <abinnet.a@gmail.com>
2018-03-10 18:23:13 -0800

    completed most of testing for making new reservation without conflicting with existing reservations

--- specs/spec_admin.rb
+++ specs/spec_admin.rb
@@ -76,103 +76,139 @@ describe 'add_reservation' do
     admin_test_2.reservations[1].room_id.must_equal 2
   end
 
-  describe "bookings_by_date" do
+  it "takes reservation check in and check out date, assigns a room and makes a reservation" do
+    admin_test_2 = Admin.new
 
-    before do
-      booking_dates = [
-        ['2018-1-10', '2018-3-5'], ['2018-1-19', '2018-5-2'], ['2018-1-10', '2018-5-5']
-      ]
+    admin_test_2.bookings_by_date(@date1).include?(1).must_equal false
 
-      @admin_test = Admin.new
+    admin_test_2.add_reservation(@date1, @date2, 1)
 
-      booking_dates.each do |dates|
-        @admin_test.add_reservation(dates[0], dates[1], 1)
-      end
-
-      @date1 = '2018-1-15'
-    end
-
-    it "returns an array" do
-      bookings_on_test_day = @admin_test.bookings_by_date(@date1)
-
-      bookings_on_test_day.must_be_instance_of Array
-      bookings_on_test_day[0].must_be_instance_of Reservation
-    end
-
-    it "returns array of reservation instances" do
-      bookings_on_test_day = @admin_test.bookings_by_date(@date1)
-
-      bookings_on_test_day.must_be_instance_of Array
-      bookings_on_test_day[0].must_be_instance_of Reservation
-    end
-
-    it "reservation check_in/check_out date range includes date argument input" do
-      bookings_on_test_day = @admin_test.bookings_by_date(@date1)
-
-      Date.parse(@date1).between?(bookings_on_test_day[1].start_date, bookings_on_test_day[1].end_date).must_equal true
-    end
-
-    it "returns an empty array if no reservation dates intersect with booking date argument given" do
-      bookings_on_test_day = @admin_test.bookings_by_date('2019-1-10')
-
-      bookings_on_test_day.must_be_instance_of Array
-
-      bookings_on_test_day.count.must_equal 0
-    end
+    admin_test_2.bookings_by_date(@date1)[0].room_id.must_equal 1
   end
 
-  describe "available_rooms" do
-    before do
-      booking_dates = [
-        ['2018-1-10', '2018-3-5'], ['2018-1-19', '2018-5-2'], ['2018-1-10', '2018-5-5']
-      ]
+  it "assigns available room" do
+    admin_test_2 = Admin.new
 
-      @admin_test = Admin.new
+    admin_test_2.available_rooms(@date1, @date2).include?(1).must_equal true
 
-      room_id = 1
+    admin_test_2.add_reservation(@date1, @date2, 1)
 
-      booking_dates.each do |dates|
-        @admin_test.add_reservation(dates[0], dates[1], room_id)
-        room_id += 1
+    admin_test_2.available_rooms(@date1, @date2).include?(1).must_equal false
+
+  end
+
+  it "assigned room is not assigned again after being reserved" do
+    admin_test_2 = Admin.new
+
+    available_rooms = admin_test_2.bookings_by_date(@date1 << 5).dup
+
+    bookings_2 = admin_test_2.reservations
+
+    proc {
+      admin_test_2.add_reservation(@date1 << 5, @date2 << 5, 1)}.must_raise ArgumentError
+    end
+
+    it "raises error if no availble room for check-in/check-out range" do
+
+    end
+
+    describe "bookings_by_date" do
+
+      before do
+        booking_dates = [
+          ['2018-1-10', '2018-3-5'], ['2018-1-19', '2018-5-2'], ['2018-1-10', '2018-5-5']
+        ]
+
+        @admin_test = Admin.new
+
+        booking_dates.each do |dates|
+          @admin_test.add_reservation(dates[0], dates[1], 1)
+        end
+
+        @date1 = '2018-1-15'
+      end
+
+      it "returns an array" do
+        bookings_on_test_day = @admin_test.bookings_by_date(@date1)
+
+        bookings_on_test_day.must_be_instance_of Array
+        bookings_on_test_day[0].must_be_instance_of Reservation
+      end
+
+      it "returns array of reservation instances" do
+        bookings_on_test_day = @admin_test.bookings_by_date(@date1)
+
+        bookings_on_test_day.must_be_instance_of Array
+        bookings_on_test_day[0].must_be_instance_of Reservation
+      end
+
+      it "reservation check_in/check_out date range includes date argument input" do
+        bookings_on_test_day = @admin_test.bookings_by_date(@date1)
+
+        Date.parse(@date1).between?(bookings_on_test_day[1].start_date, bookings_on_test_day[1].end_date).must_equal true
+      end
+
+      it "returns an empty array if no reservation dates intersect with booking date argument given" do
+        bookings_on_test_day = @admin_test.bookings_by_date('2019-1-10')
+
+        bookings_on_test_day.must_be_instance_of Array
+
+        bookings_on_test_day.count.must_equal 0
       end
     end
 
-    it "takes valid checkin/checkout dates returns an array of room ids available for the date range" do
-      rooms = @admin_test.available_rooms('2018-1-5', '2018-1-9')
-      rooms.count.must_equal 20
-    end
+    describe "available_rooms" do
+      before do
+        booking_dates = [
+          ['2018-1-10', '2018-3-5'], ['2018-1-19', '2018-5-2'], ['2018-1-10', '2018-5-5']
+        ]
 
-    it "rooms are available for new reservation check in on last day of existing reservation" do
-      rooms = @admin_test.available_rooms('2018-1-5', '2018-1-10')
-      rooms.count.must_equal 20
-    end
+        @admin_test = Admin.new
 
-    it "rooms are available for new reservation checkout on first day of existing reservation" do
-      rooms = @admin_test.available_rooms('2018-5-5', '2018-5-10')
-      rooms.count.must_equal 20
-    end
+        room_id = 1
 
-    it "returns list of available rooms without reservations dates that coincide with new reservation date range." do
-      rooms = @admin_test.available_rooms('2018-1-21', '2018-2-15')
-      rooms.count.must_equal 17
-    end
-
-    it "returns empty array of available rooms if hotel is fully booked during new reservation date range." do
-
-      date1 = '2018-1-10'
-      date2 = '2018-3-5'
-
-      full_hotel_test = Admin.new
-
-      20.times do |index|
-        full_hotel_test.add_reservation(date1, date2, index + 1)
+        booking_dates.each do |dates|
+          @admin_test.add_reservation(dates[0], dates[1], room_id)
+          room_id += 1
+        end
       end
 
-      rooms = full_hotel_test.available_rooms('2018-1-21', '2018-2-15')
+      it "takes valid checkin/checkout dates returns an array of room ids available for the date range" do
+        rooms = @admin_test.available_rooms('2018-1-5', '2018-1-9')
+        rooms.count.must_equal 20
+      end
 
-      rooms.count.must_equal 0
+      it "rooms are available for new reservation check in on last day of existing reservation" do
+        rooms = @admin_test.available_rooms('2018-1-5', '2018-1-10')
+        rooms.count.must_equal 20
+      end
 
-      rooms.must_be_instance_of Array
+      it "rooms are available for new reservation checkout on first day of existing reservation" do
+        rooms = @admin_test.available_rooms('2018-5-5', '2018-5-10')
+        rooms.count.must_equal 20
+      end
+
+      it "returns list of available rooms without reservations dates that coincide with new reservation date range." do
+        rooms = @admin_test.available_rooms('2018-1-21', '2018-2-15')
+        rooms.count.must_equal 17
+      end
+
+      it "returns empty array of available rooms if hotel is fully booked during new reservation date range." do
+
+        date1 = '2018-1-10'
+        date2 = '2018-3-5'
+
+        full_hotel_test = Admin.new
+
+        20.times do |index|
+          full_hotel_test.add_reservation(date1, date2, index + 1)
+        end
+
+        rooms = full_hotel_test.available_rooms('2018-1-21', '2018-2-15')
+
+        rooms.count.must_equal 0
+
+        rooms.must_be_instance_of Array
+      end
     end
   end
-end
